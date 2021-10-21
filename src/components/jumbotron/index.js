ccd2f0f0593ebb8a03f8782a41bb087ebd2ef8c7
rikicop <ruperto1@protonmail.com>
2021-10-21 12:56:36 -0400

    Styled Components

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -1,10 +1,42 @@
 import React from "react";
-import { Inner } from "./styles/jumbotron";
+import {
+  Container,
+  Inner,
+  Item,
+  Pane,
+  Title,
+  SubTitle,
+  Image,
+} from "./styles/jumbotron";
 
-export default function Jumbotron() {
+export default function Jumbotron({
+  children,
+  direction = "row",
+  ...restProps
+}) {
   return (
-    <Inner>
-      <p>Hello Again!</p>
-    </Inner>
+    <Item direction={direction} {...restProps}>
+      <Inner>{children}</Inner>
+    </Item>
   );
 }
+
+Jumbotron.Container = function JumbotronContainer({ children, ...restProps }) {
+  return <Container {...restProps}>{children}</Container>;
+};
+
+Jumbotron.Pane = function JumbotronPane({ children, ...restProps }) {
+  return <Pane {...restProps}>{children}</Pane>;
+};
+
+Jumbotron.Title = function JumbotronTitle({ children, ...restProps }) {
+  return <Title {...restProps}>{children}</Title>;
+};
+
+Jumbotron.SubTitle = function JumbotronSubTitle({ children, ...restProps }) {
+  return <SubTitle {...restProps}>{children}</SubTitle>;
+};
+
+Jumbotron.Image = function JumbotronImage({ ...restProps }) {
+  return <Image {...restProps} />;
+};
