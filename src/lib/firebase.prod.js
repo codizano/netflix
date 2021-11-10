18e9d2898f31419967c37f1660400c837229727a
rikicop <ruperto1@protonmail.com>
2021-11-10 14:49:37 -0400

    form sign in

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -1,9 +1,19 @@
-import Firebase from "firebase/app";
-import "firebase/firestore";
-import "firebase/auth";
+import Firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+//import { seedDatabase } from "../seed";
 
-const config = {};
+const config = {
+  apiKey: "AIzaSyBHsrJWN9hIq3ZEe8kvUm6BHlBUekPpcBI",
+  authDomain: "netflix-39588.firebaseapp.com",
+  projectId: "netflix-39588",
+  storageBucket: "netflix-39588.appspot.com",
+  messagingSenderId: "503834225964",
+  appId: "1:503834225964:web:bc65c96996db62fca3d089",
+};
 
 const firebase = Firebase.initializeApp(config);
+
+//seedDatabase(firebase);
 
 export { firebase };
--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,5 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import { FooterContainer } from "../containers/footer";
+import { HeaderContainer } from "../containers/header";
+import { Form } from "../components";
 
-export default function Signup() {
-  return <div>Hola desde Sign in!!</div>;
+export default function Signin() {
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const isInvalid = password === "" || emailAddress === "";
+
+  const handleSignin = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign In</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+          <Form.Base onSubmit={handleSignin} metho="POST"></Form.Base>
+          <Form.Input
+            placeholder="Email address"
+            value={emailAddress}
+            onChange={({ target }) => setEmailAddress(target.value)}
+          />
+          <Form.Input
+            type="password"
+            value={password}
+            autoComplete="off"
+            placeholder="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+          <Form.Submit disabled={isInvalid} type="submit" data-testid="sign-in">
+            Sign In
+          </Form.Submit>
+        </Form>
+      </HeaderContainer>
+      <FooterContainer />
+    </>
+  );
 }
